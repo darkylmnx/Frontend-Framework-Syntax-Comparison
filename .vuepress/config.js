fe6765a7a73b0d4957785c7adb3300a55129f5b0
ariel <darkun91@gmail.com>
2018-05-12 04:49:33 +0200

    :wrench: added title + description

---
+++ .vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'Hikaku - Frontend Frameworks Comparison',
+  description: 'Hikaku project helps you to compare frontend frameworks based on syntax and usage. You will also have ready-to-use code snippets.'
+}
